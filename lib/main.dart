ffa45d0911a161cebf2102e9b56f841e5dc6883f
Youssef Mohamed <125169424+youssef-mohamed07@users.noreply.github.com>
2025-10-02 21:05:11 +0300

    Revert "Features/splash"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'core/splash/Voclio_splash_screen.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -15,14 +13,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is tshe theme of your application.
-        //ssssddsdsيسيسdds
-        // TRY THIS: Try ruسسشيnning your application with "flutter run". You'll see
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invokedd "hot reload" (save your changes or press the "hot
-        // reload" button in a Fluttesdasaddssdasdr-supported IDE, or press "r" if you used
-        // the command line to start sathe app).
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
         //
         // Notice that the counter didn't reset back to zero; the application
         // state is not lost during the reload. To reset the state, use hot
@@ -32,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: VoclioSplashScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
